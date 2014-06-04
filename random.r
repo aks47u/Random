7de1a51ef5ce1a497d8f997a2b21b55c55844d65
Conor Fitzpatrick <aks47u@hotmail.co.uk>
2014-06-04 21:00:15 +0100

    Update random.r

--- random.r
+++ random.r
@@ -1,42 +1,42 @@
-		ABchoice = function(A, B, nbChancesA, outOfMe) {
-			r=floor(runif(1,1,outOfMe+1))
-					choice=A
-					if(r>nbChancesA){
-						choice=B
-					}
-			return(choice)
-		}
+ABchoice = function(A, B, nbChancesA, outOfMe) {
+	r=floor(runif(1,1,outOfMe+1))
+			choice=A
+			if(r>nbChancesA){
+				choice=B
+			}
+	return(choice)
+}
 
-		simulation = function(suggested, ntries) {
-			margin=c()
-					for(i in 1:ntries){
-						margin[i]=0
-								if(suggested=="chips"){
-									choice=ABchoice("chips","bread",2,3)
-								} else {
-									choice=ABchoice("chips","bread",1,3)
-								}
-						if(choice=="chips"){
-							margin[i]=margin[i]+0.2
-									drink=ABchoice("soda","water",2,3)
-									if(drink=="soda"){
-										margin[i]=margin[i]+0.1
-									} else {
-										margin[i]=margin[i]-0.1
-									}
+simulation = function(suggested, ntries) {
+	margin=c()
+			for(i in 1:ntries){
+				margin[i]=0
+						if(suggested=="chips"){
+							choice=ABchoice("chips","bread",2,3)
 						} else {
-							margin[i]=margin[i]+0.3
-									drink=ABchoice("soda","water",1,3)
-									if(drink=="soda"){
-										margin[i]=margin[i]+0.1
-									} else {
-										margin[i]=margin[i]-0.1
-									}
+							choice=ABchoice("chips","bread",1,3)
 						}
-					}
-			return(margin)
-		}
+				if(choice=="chips"){
+					margin[i]=margin[i]+0.2
+							drink=ABchoice("soda","water",2,3)
+							if(drink=="soda"){
+								margin[i]=margin[i]+0.1
+							} else {
+								margin[i]=margin[i]-0.1
+							}
+				} else {
+					margin[i]=margin[i]+0.3
+							drink=ABchoice("soda","water",1,3)
+							if(drink=="soda"){
+								margin[i]=margin[i]+0.1
+							} else {
+								margin[i]=margin[i]-0.1
+							}
+				}
+			}
+	return(margin)
+}
 
 
-		hist(simulation("chips",100000),breaks=seq(-0.1,0.5,0.01))
-		hist(simulation("bread",100000),breaks=seq(-0.1,0.5,0.01))
+hist(simulation("chips",100000),breaks=seq(-0.1,0.5,0.01))
+hist(simulation("bread",100000),breaks=seq(-0.1,0.5,0.01))
